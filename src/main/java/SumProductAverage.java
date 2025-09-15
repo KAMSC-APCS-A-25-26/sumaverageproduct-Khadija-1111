4f8a4e236edd11ec21c24df1b641d81506d76de2
Khadija-1111 <sfkhadija.edu@gmail.com>
2025-09-15 13:46:08 -0400

    Update SumProductAverage.java

--- src/main/java/SumProductAverage.java
+++ src/main/java/SumProductAverage.java
@@ -18,5 +18,32 @@ import java.util.Scanner;
 public class SumProductAverage {
     public static void main(String[] args) {
         // TODO
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\nSyeda Khadija\nAPCS-A\nLewis 2.2-2.3 Arithmetic Program\n\n");
+
+    // prompts the user for the number of times the program will run
+        System.out.print("Enter the number of runs:  ");
+        int numRuns = sc.nextInt();
+
+    // runs the program for a number of times given by the user
+        for(int j=0; j<numRuns; j++)
+        {
+            // prompts the user for three numbers and stores their values into variables
+            System.out.print("\n\nEnter three numbers:  ");
+            double num1 = sc.nextDouble();
+            double num2 = sc.nextDouble();
+            double num3 = sc.nextDouble();
+
+            // calculates the average, sum, and product of the three numbers
+            double average = (num1 + num2 + num3) / 3;
+            double sum = (num1 + num2 + num3);
+            double product = (num1 * num2 * num3);
+
+            // prints out the average, sum, and product
+            System.out.println("\nYour average is " + average);
+            System.out.println("Your sum is " + sum);
+            System.out.println("Your product is " + product);
+        }
     }
 }
